c39f25624d6222275834809baedebc348ad0388a
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-06-16 15:48:01 +0900

    add output log

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
+
+const fs = require('fs');
 const express = require('express');
 const dayjs = require('dayjs');
 
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 const app = express();
@@ -11,6 +13,12 @@ app.get('/', (req, res) => {
   const time = dayjs().format('YYYY-MM-DD HH:mm:ss');
   const message = `${time} Container Access!!!`;
   console.log(message);
+
+  fs.appendFileSync('output.log', message, error => {
+    if (error) throw new Error(error);
+    console.log('ファイルが正常に出力されました。');
+  });
+
   res.send('Hello World');
 });
 
